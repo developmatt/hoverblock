c6d81634c60c70ee1cba3a026cc581e5d80f2fa0
Matheus Godoi <matheus.godoi@vianetbrasil.com.br>
2018-09-04 13:29:17 -0300

    Modules be activate by data attribute

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ window.onload = function(){
     let fillerBlock = document.createElement('div');
     fillerBlock.id = 'hoverblock-filler';
     fillerBlock.className = 'hoverblock-filler';
-    fillerBlock.innerHTML = '<p id="hoverblock-filler-text" class="hoverblock-filler-text"></p>';
+    fillerBlock.attr = ('data-hoverblock', 'filler');
+    fillerBlock.innerHTML = '<p id="hoverblock-filler-text" class="hoverblock-filler-text" data-hoverblock-filler="text"></p>';
     fillerBlock.style = fillerBlockStyle;
 
-    document.getElementsByClassName('hoverblock-content')[0].appendChild(fillerBlock);
-
     let content = document.querySelector("[data-hoverblock=content]");
+    content.appendChild(fillerBlock);
     content.style = hoverBlockContent;
 
     let fillableBlocks = document.querySelectorAll("[data-hoverblock=fillable-block]");
@@ -20,7 +20,7 @@ window.onload = function(){
 
     fillableBlocks.forEach((item, index) => {
         item.addEventListener("mouseover", function(){
-            document.getElementById("hoverblock-filler-text").innerText = this.getAttribute('data-hoverblock-text');
+            document.querySelector("[data-hoverblock-filler=text]").innerText = this.getAttribute('data-hoverblock-text');
             fillerBlock.style.top = this.offsetTop + 'px';
             fillerBlock.style.left = this.offsetLeft + 'px';
             fillerBlock.style.width = this.offsetWidth + 'px';
